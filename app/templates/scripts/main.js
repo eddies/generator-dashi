c4c6f64710742ace87451587767cf08d267bf962
Edwin Shin <edwin.shin@yourmediashelf.com>
2015-05-15 17:10:55 +0800

    Fixes #6 (Allow linking to tabs).
    Backport of fix from eddies/home-dashi#6:
    
    Adds a new function to app/scripts/main.js that will read the tab based on the
    window.location.hash and show it.
    
    I chose to comment out functionality that would send the hash to the address bar
    on click: e.g. selecting the sidebar link for "Domestic workers" would update
    the location from http://example.com to http://example.com/#domestic. Instead,
    the location bar just remains as-is, regardless of which sidebar tab link is
    clicked.

--- app/templates/scripts/main.js
+++ app/templates/scripts/main.js
@@ -1,13 +1,12 @@
 'use strict';
 
-/*jshint unused:false*/
-
 /**
  * Dynamically load content into Bootstrap tabs
  */
 $(function() {
   
   // see: https://stackoverflow.com/questions/28322636/synchronous-xmlhttprequest-warning-and-script
+  /*jshint unused:false*/
   $.ajaxPrefilter(function( options, originalOptions, jqXHR ) {
       options.async = true;
   });
@@ -27,4 +26,40 @@ $(function() {
       // });
     }
   });
+});
+
+/**
+ * Allow linking to Bootstrap tabs
+ *
+ * see: http://stackoverflow.com/a/26430204/3811916
+ * see: https://github.com/twbs/bootstrap/issues/2415
+ */
+$(function() {
+    // cache the id
+    var navbox = $('.nav-pills');
+
+    // activate tab on click
+    // navbox.on('click', 'a', function (e) {
+    //   var $this = $(this);
+    //   // prevent the Default behavior
+    //   e.preventDefault();
+    //   // send the hash to the address bar
+    //   window.location.hash = $this.attr('href');
+    //   // activate the clicked tab
+    //   $this.tab('show');
+    // });
+
+    // will show tab based on hash
+    function refreshHash() {
+      navbox.find('a[href="' + window.location.hash + '"]').tab('show');
+    }
+
+    // show tab if hash changes in address bar
+    $(window).bind('hashchange', refreshHash);
+
+    // read hash from address bar and show it
+    if(window.location.hash) {
+      // show tab on load
+      refreshHash();
+    }
 });
